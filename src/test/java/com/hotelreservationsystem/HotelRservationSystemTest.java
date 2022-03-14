bfc078c1ab37b7a1177ddcfb1aa7e7ab6a5c578c
Pooja Sree <you@example.com>
2022-03-14 09:40:51 +0530

    added:welcome message

---
+++ src/test/java/com/hotelreservationsystem/HotelRservationSystemTest.java
@@ -0,0 +1,8 @@
+package com.hotelreservationsystem;
+
+public class HotelRservationSystemTest {
+    public static void main(String[] args) {
+        System.out.println("welcome to hotel reservation");
+    }
+
+}
